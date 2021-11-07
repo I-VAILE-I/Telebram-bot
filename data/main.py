836565b89cb479acf65d2369711daa9d8de81df9
VAILE <anchenko56@mail.ru>
2021-11-07 17:21:00 +0300

    writed pythone code TG bot

--- data/main.py
+++ data/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import telebot
+bot = telebot.TeleBot('%2062309923:AAExMItj8JzG6w_ybDm0dquLHPeEiwCVbZQ%')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        
+bot.polling(none_stop=True, interval=0)
