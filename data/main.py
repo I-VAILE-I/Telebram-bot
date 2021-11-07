878388c2967eb4de0552739e1917a66925c50681
VAILE <anchenko56@mail.ru>
2021-11-07 17:54:32 +0300

    add gitignore

--- data/main.py
+++ data/main.py
@@ -1,15 +1,20 @@
+import telebot
 
-import telebot;
-bot = telebot.TeleBot('%2062309923:AAExMItj8JzG6w_ybDm0dquLHPeEiwCVbZQ%');
-
+bot = telebot.TeleBot('2062309923:AAExMItj8JzG6w_ybDm0dquLHPeEiwCVbZQ')
 @bot.message_handler(content_types=['text'])
-
 def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    if message.text == "Проверить юбилейные билеты" \
+    or message.text == "проверить юбилейные билеты" \
+    or message.text == "пюб":
+        if tickets == True:
+            bot.send_message(message.from_user.id, "Привет, я нашел вот такие юбилейные билеты")
+        else:
+            bot.send_message(message.from_user.id, "Привет, я нашел вот такие юбилейные билеты")
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+        bot.send_message(message.from_user.id, "Напиши")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+tickets = True
 
 bot.polling(none_stop=True, interval=0)
