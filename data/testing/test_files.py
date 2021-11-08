7b9582ee4d8a5378cf510e735f177994db319103
VAILE <anchenko56@mail.ru>
2021-11-08 17:52:13 +0300

    working parser

--- data/testing/test_files.py
+++ data/testing/test_files.py
@@ -4,5 +4,7 @@ from bs4 import BeautifulSoup
 url = 'https://quotes.toscrape.com/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_='text')
 
-print(soup)
+for quote in quotes:
+    print(quote.text)
