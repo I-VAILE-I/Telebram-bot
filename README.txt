5c470dc4112a93c721453a5ea362491dafe24c2c
VAILE <anchenko56@mail.ru>
2021-11-08 17:30:38 +0300

    change test_file.py add README.txt

---
+++ README.txt
@@ -0,0 +1,6 @@
+instelled lib
+
+pip install pytelegrambotapi
+pip install telebot
+pip install beautifulsoup4
+pip install lxml
--- data/testing/test_files.py
+++ data/testing/test_files.py
@@ -1,12 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
-bank_id = 1771062 #ID банка на сайте banki.ru
-page=1
-max_page=10
+url = 'https://quotes.toscrape.com/'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
 
-url = 'https://www.banki.ru/services/questions-answers/?id=%d&p=%d' % (bank_id, page)
-r = requests.get(url)
-with open('test.html', 'w') as output_file:
-  output_file.write(r.text)
+print(soup)
