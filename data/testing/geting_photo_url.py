13bef528dbfd406c86f5edc940eb35d56b4e0700
VAILE <anchenko56@mail.ru>
2021-11-10 20:52:22 +0300

    working photo url parser

--- data/testing/geting_photo_url.py
+++ data/testing/geting_photo_url.py
@@ -10,22 +10,25 @@ img_url = []
 photo = []
 znach = False
 otriv = 0
-for i in range(2):
-    b = str(photo_url[i]) + '"'
+kolvo = 1 * 2 # 1 text about ticket = 2 tickets photo
+for i in range(kolvo):
+    otriv = 0
+    rashodnik = str(photo_url[i]) + '"'
     #print(b)
-    b.split('"')
-    for j in range(len(b)):
+    rashodnik.split('"')
+    for j in range(len(rashodnik)):
         #print(b[j])
-        if b[j] == '"' and otriv != 1:
+        if rashodnik[j] == '"' and otriv != 1:
             znach = True
             otriv += 1
             inde_X = j + 1
             while znach == True:
-                img_url.append(b[inde_X])
+                img_url.append(rashodnik[inde_X])
                 inde_X += 1
-                if b[inde_X] == '"':
+                if rashodnik[inde_X] == '"':
                     znach = False
             #print(*img_url, sep='')
             photo_url_ = 'https://mosmetro.ru' + ''.join(img_url)
             photo.append(photo_url_)
             img_url.clear()
+print(photo)
