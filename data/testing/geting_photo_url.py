9a1c8bb2bd5b0414d532eab3b9b0f7de1051904d
VAILE <anchenko56@mail.ru>
2021-11-10 20:46:00 +0300

    working for first url

--- data/testing/geting_photo_url.py
+++ data/testing/geting_photo_url.py
@@ -7,20 +7,25 @@ soup = BeautifulSoup(response.text, 'lxml')
 photo_url = soup.find_all(width="333")
 print(photo_url)
 img_url = []
+photo = []
 znach = False
-
+otriv = 0
 for i in range(2):
     b = str(photo_url[i]) + '"'
     #print(b)
     b.split('"')
     for j in range(len(b)):
         #print(b[j])
-        if b[j] == '"':
+        if b[j] == '"' and otriv != 1:
             znach = True
-            inde_X = j
+            otriv += 1
+            inde_X = j + 1
             while znach == True:
+                img_url.append(b[inde_X])
                 inde_X += 1
                 if b[inde_X] == '"':
                     znach = False
-                img_url.append(b[inde_X])
-            print(*img_url)
+            #print(*img_url, sep='')
+            photo_url_ = 'https://mosmetro.ru' + ''.join(img_url)
+            photo.append(photo_url_)
+            img_url.clear()
