d55255df48155992ccb1cf23867b3892e81f2b6e
VAILE <anchenko56@mail.ru>
2021-11-21 22:05:25 +0300

    e

--- data/main.py
+++ data/main.py
@@ -1,63 +1,115 @@
+import dataclasses
+from typing import List
+import bs4
 import telebot
 import requests
 from bs4 import BeautifulSoup
 import datetime
-import calendar
-from data.testing.web_site_parser import availability_of_tickets
-from data.testing.geting_photo_url import tickets_photos
+from telebot.types import Message
 
 token = open("token.txt", "r")
 bot = telebot.TeleBot(token.read())
 
-url = 'https://mosmetro.ru/passengers/information/special-tickets/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-tickets_info = soup.find_all('p')
+URL = 'https://mosmetro.ru/passengers/information/special-tickets/'
 
-today_date = datetime.date.isoformat(datetime.date.today()).split('-')
-today_date.reverse()
-
-loop_list = []
-
-# day = 4 -1
-# month = 11
-# year = 2021
-
-day = int(today_date[0]) -1
-month = int(today_date[1])
-year = int(today_date[2])
-
-last_day = calendar.monthrange(year,month)[-1]
-week_list = []
-
-url = 'https://mosmetro.ru/passengers/information/special-tickets/'
-photo_response = requests.get(url)
-photo_soup = BeautifulSoup(photo_response.text, 'lxml')
-photo_url = photo_soup.find_all(width="333")
-
-img_url = []
-photo = []
-znach = False
-otriv = 0
-kolvo = 1 * 2
-
-itog_date = availability_of_tickets(tickets_info, loop_list, day, month, year, last_day, week_list)
-itog_photo = tickets_photos(photo_url, otriv, kolvo, znach, photo, img_url)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id,'Вас приветствует Бот Эйситристик, здесь вы сможете узнавать о выходе юбилейных единых билетов в Московском метрополитене. \nЧто бы проверить информацию о билетах на ближайшие 7 дней, напишите команду "Проверить юбилейные билеты" или сокращенно "пюб".')
+    bot.send_message(message.chat.id, """
+    Вас приветствует Бот Эйситристик, здесь вы сможете узнавать о выходе юбилейных единых билетов в
+    Московском метрополитене. Что бы проверить информацию о билетах на ближайшие 7 
+    дней, напишите команду "Проверить юбилейные билеты" или сокращенно "пюб".
+    """)
+
+
+@dataclasses.dataclass
+class Ticket:
+    web_tag: bs4.element.Tag
+    name: str
+    date: datetime
+    images: str = dataclasses.field(default_factory=list)
+
+
+# tickets_all_on_page[0].web_tag.find_next('img').get('src') - для картинок
+
+def convert_str_to_date(date_time_str: str) -> datetime:
+    return datetime.datetime.strptime(date_time_str, '%d.%m.%Y')
+
+
+def get_date_from_ticket_text(text: str) -> str:
+    start_index = text.find('(') + 1
+    end_index = start_index + 10
+    return text[start_index:end_index]
+
+
+def get_name_from_ticket_text(text: str) -> str:
+    start_index = text.find('«') + 1
+    end_index = text.find('»') - 1
+    return text[start_index:end_index]
+
+
+def get_images(text: str) -> str:
+    start_index = text.find('=') + 2
+    end_index = text.find('.jpeg')
+    return text[start_index:end_index]
+
+
+def get_all_tickets_on_page(url: str) -> List[Ticket]:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    tickets_info = soup.find_all('p')
+    tickets: List[Ticket] = []
+    for ticket in tickets_info:
+        if 'Билет' in ticket.text:
+            tickets.append(
+                Ticket(
+                    web_tag=ticket,
+                    name=get_name_from_ticket_text(ticket.text),
+                    date=convert_str_to_date(
+                        get_date_from_ticket_text(ticket.text)
+                    ),
+                    images=get_images(ticket.text)
+                )
+            )
+    return tickets
+
+
+tickets = get_all_tickets_on_page(url=URL)
+start = convert_str_to_date('11.11.2020') - datetime.timedelta(days=1)
+end = start + datetime.timedelta(days=7)
+
+
+def get_tickets_by_date(
+        tickets: List[Ticket],
+        start: datetime.datetime,
+        end: datetime.datetime,
+) -> List[Ticket]:
+    l = []
+
+    for d in tickets:
+        if d.date >= start:
+            if d.date <= end:
+                l.append(d)
+
+    return l
+
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
+def get_text_messages(message: Message):
     if message.text == "Проверить юбилейные билеты" or message.text == "проверить юбилейные билеты" or message.text == "пюб":
+        tickets_all_on_page: List[Ticket] = get_all_tickets_on_page(url=URL)
+        tickets_on_week: List[Ticket] = get_tickets_by_date(tickets=tickets, start=start, end=end)
         bot.send_message(message.from_user.id, "Привет, я нашел вот такие юбилейные билеты:")
-        bot.send_message(message.chat.id, itog_date)
-        for i in range(len(itog_photo)):
-            bot.send_photo(message.from_user.id, itog_photo[i], "Их внешний вид.") #bot.send_photo(id, photo, caption='текст')
+        for i in range(len(tickets_on_week)):
+            ticket_name = tickets_on_week[i].name
+            ticket_date = tickets_on_week[i].date
+            bot.send_message(message.from_user.id, ticket_name, ticket_date.strftime('%d.%m.%Y'))
+            bot.send_message(message.from_user.id, tickets_all_on_page[0].web_tag.find_next('img').get('src'))
+
     elif message.text == "/help":
         bot.send_message(message.from_user.id, 'Напишите "Проверить юбилейные билеты" или сокращенно "пюб"')
     else:
         bot.send_message(message.from_user.id, "Команда не распознана. Напишите /help.")
+
 
 bot.polling(none_stop=True, interval=0)
--- data/testing/geting_photo_url.py
+++ data/testing/geting_photo_url.py
@@ -4,24 +4,24 @@ import requests
 from bs4 import BeautifulSoup
 from data.testing.web_site_parser import availability_of_tickets
 
-def tickets_photos(photo_url, otriv, kolvo, znach, photo, img_url):
+def tickets_photos(photo_url, kolvo, znach, photo, img_url):
     """
     Функция делает то-то
     """
     for i in range(kolvo):
-        otriv = 0
+        fragment = 0
         rashodnik = str(photo_url[i]) + '"'
         rashodnik.split('"')
         for j in range(len(rashodnik)):
-            if rashodnik[j] == '"' and otriv != 1:
-                znach = True
-                otriv += 1
+            if rashodnik[j] == '"' and fragment != 1:
+                meaning = True
+                fragment += 1
                 inde_X = j + 1
-                while znach == True:
+                while meaning == True:
                     img_url.append(rashodnik[inde_X])
                     inde_X += 1
                     if rashodnik[inde_X] == '"':
-                        znach = False
+                        meaning = False
                 photo_url_ = 'https://mosmetro.ru' + ''.join(img_url)
                 photo.append(photo_url_)
                 img_url.clear()
@@ -62,25 +62,22 @@ year = 2020
 
 last_day = calendar.monthrange(year,month)[-1]
 week_list = []
-print(availability_of_tickets(tickets_info, loop_list, day, month, year, last_day, week_list, name_tickets))
-# itog_tickets = availability_of_tickets(tickets_info, loop_list, day, month, year, last_day, week_list, name_tickets)
-itog_tickets = ['Билет «Доноры надежды» (16.11.2020)', 'Билет «10 лет новой схеме метро» (14.11.2020)', 'Билет «День качества» (12.11.2020)', 'Билет «ВХУТЕМАС 100» (11.11.2020)']
+# print(availability_of_tickets(tickets_info, loop_list, day, month, year, last_day, week_list, name_tickets))
+itog_tickets = availability_of_tickets(tickets_info, loop_list, day, month, year, last_day, week_list, name_tickets)
+# itog_tickets = ['Билет «Доноры надежды» (16.11.2020)', 'Билет «10 лет новой схеме метро» (14.11.2020)', 'Билет «День качества» (12.11.2020)', 'Билет «ВХУТЕМАС 100» (11.11.2020)']
 
 
 img_url = []
 photo = []
-znach = False
-znach2 = 0
+meaning = False
+meaning2 = 0
 otriv = 0
 kolvo = len(itog_tickets) *2 # 1 text about ticket = 2 tickets photo
 id_photo = []
 used_ide = []
-# print(itog_tickets, len(itog_tickets))
-# for id in range(len(itog_tickets)):
-#     id_photo.append(only_tickets.index(str(itog_tickets[id]))-1)
-# print(id_photo)
+
 using_id = 0
-print(availability_of_tickets(tickets_info, loop_list, day, month, year, last_day, week_list, name_tickets))
+
 for i in range(len(itog_tickets)*2):
     for id in range(len(itog_tickets)):
         if using_id != len(itog_tickets):
@@ -89,21 +86,21 @@ for i in range(len(itog_tickets)*2):
             using_id += 1
         else:
             break
-    otriv = 0
+    fragment = 0
     print(id_photo)
     rashodnik = str(photo_url[id_photo[i]]) + '"'
     print(i, rashodnik)
     rashodnik.split('"')
     for j in range(len(rashodnik)):
-        if rashodnik[j] == '"' and otriv != 1:
-            znach = True
+        if rashodnik[j] == '"' and fragment != 1:
+            meaning = True
             otriv += 1
             inde_X = j + 1
-            while znach == True:
+            while meaning == True:
                 img_url.append(rashodnik[inde_X])
                 inde_X += 1
                 if rashodnik[inde_X] == '"':
-                    znach = False
+                    meaning = False
             photo_url_ = 'https://mosmetro.ru' + ''.join(img_url)
             photo.append(photo_url_)
             img_url.clear()
